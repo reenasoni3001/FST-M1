c9e342e2de516f9a1890bb66a42447e1f9665a31
reenasoni3001 <41507972+reenasoni3001@users.noreply.github.com>
2022-01-24 16:07:19 +0530

    Add files via upload

---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,22 @@
+	
+CREATE TABLE salesman (
+    salesman_id int,
+    salesman_name varchar2(32),
+    salesman_city varchar2(32),
+    commission int
+);
+
+INSERT ALL
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11)
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14)
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13)
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12)
+SELECT 1 FROM DUAL;
+
+-- View data from all columns
+SELECT * FROM salesman;
+
+ALTER TABLE salesman ADD grade int;
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
